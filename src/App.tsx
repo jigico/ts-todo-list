1fcdc85edcff8c8fac2a594f7fab34197d9140bc
jisu <ahteostnqls@naver.com>
2024-03-06 01:52:56 +0900

    [MOD] react-query 셋팅 및 devtools 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { ReactQueryDevtools } from "react-query/devtools";
 import { Header } from "components/Header";
+import { TodoForm } from "components/TodoForm";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Header />
-    </>
+      <TodoForm />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
