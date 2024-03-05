d1f96756309f49198c978f870674433538db12ed
jisu <ahteostnqls@naver.com>
2024-03-06 01:12:54 +0900

    Revert "[ADD] todos.ts 추가"
    This reverts commit ddc2f66f42afaca4c94426b7720b05ff264569ff.

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,22 +1,10 @@
 import React from "react";
 
 export const TodoForm = () => {
-  //todo 추가
-  const submitHandler = (e: React.FormEvent<HTMLFormElement>): void => {
-    e.preventDefault();
-    const target = e.target as typeof e.target & {
-      title: { value: string };
-      content: { value: string };
-    };
-    console.log(target.title.value);
-  };
-
   return (
-    <form onSubmit={submitHandler}>
-      <label htmlFor="title">제목</label>
-      <input type="text" id="title" name="title" placeholder="제목을 입력해주세요." />
-      <label htmlFor="content">내용</label>
-      <input type="text" id="content" name="content" placeholder="내용을 입력해주세요." />
+    <form>
+      <input type="text" />
+      <input type="text" />
       <button>추가</button>
     </form>
   );
