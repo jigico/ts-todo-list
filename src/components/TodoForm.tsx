5522bbd7162d634989ffa9c4bd59281c7eec4e08
jisu <ahteostnqls@naver.com>
2024-03-06 01:52:13 +0900

    [MOD] addTodo 기능 작업

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,10 +1,42 @@
+import { addTodo } from "api/todos";
 import React from "react";
+import { useMutation } from "react-query";
 
 export const TodoForm = () => {
+  const mutation = useMutation(addTodo);
+
+  //todo 추가
+  const submitHandler = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    const target = e.target as typeof e.target & {
+      title: { value: string };
+      content: { value: string };
+    };
+
+    if (!target.title.value.trim()) {
+      alert("제목을 입력해주세요.");
+      return;
+    }
+    if (!target.content.value.trim()) {
+      alert("내용을 입력해주세요.");
+      return;
+    }
+
+    const newTodo = {
+      title: target.title.value,
+      content: target.content.value,
+      isDone: false
+    };
+
+    mutation.mutate(newTodo);
+  };
+
   return (
-    <form>
-      <input type="text" />
-      <input type="text" />
+    <form onSubmit={submitHandler}>
+      <label htmlFor="title">제목</label>
+      <input type="text" id="title" name="title" placeholder="제목을 입력해주세요." />
+      <label htmlFor="content">내용</label>
+      <input type="text" id="content" name="content" placeholder="내용을 입력해주세요." />
       <button>추가</button>
     </form>
   );
