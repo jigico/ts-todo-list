5f6fc1e91ade082cc50359d6a06465cf52e44a60
jisu <ahteostnqls@naver.com>
2024-03-05 23:32:45 +0900

    [ADD] Header 컴포넌트 생성

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { HeaderStyle, Logo } from "./Header.style";
+
+export const Header = () => {
+  return (
+    <HeaderStyle>
+      <Logo>TODO</Logo>
+    </HeaderStyle>
+  );
+};
