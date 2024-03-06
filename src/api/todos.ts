396e8d522397cd1515b963ba4d193000dc602cb9
jisu <ahteostnqls@naver.com>
2024-03-06 12:20:33 +0900

    [MOD] getTodos 기능 추가

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -3,3 +3,12 @@ import axios from "axios";
 export const addTodo = async (newTodo: { title: string; content: string; isDone: boolean }) => {
   return axios.post(`${process.env.REACT_APP_JSON_SERVER_URL}/todos`, newTodo);
 };
+
+export const getTodos = async () => {
+  try {
+    const { data } = await axios.get(`${process.env.REACT_APP_JSON_SERVER_URL}/todos`);
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+};
