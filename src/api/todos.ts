d1ac21286a0dea883e2723aee368f8e879796141
jisu <ahteostnqls@naver.com>
2024-03-06 15:25:10 +0900

    [MOD] todo 삭제 기능 작업

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,12 +1,19 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
 //todo 추가
 export const addTodo = async (newTodo: { title: string; content: string; isDone: boolean }) => {
   return axios.post(`${process.env.REACT_APP_JSON_SERVER_URL}/todos`, newTodo);
 };
 
+type Todo = {
+  id: string;
+  title: string;
+  content: string;
+  isDone: boolean;
+};
+
 //todo 조회
-export const getTodos = async () => {
+export const getTodos = async (): Promise<Todo[] | undefined> => {
   try {
     const { data } = await axios.get(`${process.env.REACT_APP_JSON_SERVER_URL}/todos`);
     return data;
@@ -18,4 +25,9 @@ export const getTodos = async () => {
 //todo 완료 여부 변경
 export const toggleTodoDone = async (id: string, isDone: boolean) => {
   return axios.patch(`${process.env.REACT_APP_JSON_SERVER_URL}/todos/${id}`, { isDone });
+};
+
+//todo 삭제
+export const deleteTodo = async (id: string) => {
+  return axios.delete(`${process.env.REACT_APP_JSON_SERVER_URL}/todos/${id}`);
 };
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,6 @@
-import { getTodos } from "api/todos";
+import { deleteTodo, getTodos } from "api/todos";
 import React from "react";
-import { useQuery } from "react-query";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 
 type IsDone = {
   isDone: boolean;
@@ -10,6 +10,26 @@ type Todo = { id: string; title: string; content: string; isDone: boolean };
 
 export const TodoList = ({ isDone }: IsDone) => {
   const { data, isLoading, isError } = useQuery("todos", getTodos);
+  const queryClient = useQueryClient();
+  const deleteMutation = useMutation(deleteTodo, {
+    onSuccess: () => {
+      queryClient.invalidateQueries("todos");
+    }
+  });
+
+  //todo 삭제
+  const deleteTodoHandler = (id: string) => {
+    deleteMutation.mutate(id);
+  };
+
+  //todo 완료 여부 변경
+  const toggleDone = (id: string, isDone: boolean): void => {
+    // patchMutation.mutate(id, isDone);
+  };
+
+  if (isError) {
+    return <div>에러가 발생했습니다. 관리자에게 문의해주세요.</div>;
+  }
 
   if (isLoading) {
     return <div>로딩중</div>;
@@ -20,15 +40,15 @@ export const TodoList = ({ isDone }: IsDone) => {
       <h2>{isDone ? "🎉완료한 일" : "🔥해야할 일"}</h2>
       <ul>
         {data
-          .filter((todo: Todo) => todo.isDone === isDone)
+          ?.filter((todo: Todo) => todo.isDone === isDone)
           .map((todo: Todo) => {
             return (
               <li key={todo.id}>
                 {todo.title}
                 <p>{todo.content}</p>
                 <div>
-                  {todo.isDone ? <button>취소</button> : <button>완료</button>}
-                  <button>삭제</button>
+                  {todo.isDone ? <button onClick={() => toggleDone(todo.id, todo.isDone)}>취소</button> : <button>완료</button>}
+                  <button onClick={() => deleteTodoHandler(todo.id)}>삭제</button>
                 </div>
               </li>
             );
