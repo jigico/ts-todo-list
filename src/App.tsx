15963dd83ce1777abc4bec4dc19e7c2f6235beeb
jisu <ahteostnqls@naver.com>
2024-03-06 14:10:05 +0900

    [MOD] App.tsx - TodoList  해야할 일, 완료한 일 컴포넌트로 분리 작업

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { ReactQueryDevtools } from "react-query/devtools";
 import { Header } from "components/Header";
 import { TodoForm } from "components/TodoForm";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { TodoList } from "components/TodoList";
+import { Layout } from "components/Layout";
 
 const queryClient = new QueryClient();
 
@@ -9,7 +11,11 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Header />
-      <TodoForm />
+      <Layout>
+        <TodoForm />
+        <TodoList isDone={false} />
+        <TodoList isDone={true} />
+      </Layout>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
